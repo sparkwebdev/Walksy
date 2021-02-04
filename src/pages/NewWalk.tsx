f09b12f762f79cb78c82764e8913f55434432662
Steven Park <info@stevenpark.co.uk>
2021-02-04 17:16:29 +0000

    Add pedometer/distance and Progress

--- src/pages/NewWalk.tsx
+++ src/pages/NewWalk.tsx
@@ -1,12 +1,7 @@
-import React, { useState, useRef, useContext } from "react";
+import React, { useState, useContext } from "react";
 import {
   IonPage,
-  IonHeader,
-  IonToolbar,
-  IonTitle,
   IonContent,
-  IonButtons,
-  IonBackButton,
   IonGrid,
   IonRow,
   IonCol,
@@ -21,14 +16,26 @@ import { useHistory } from "react-router-dom";
 
 import WalksContext, { WalkType } from "../data/walks-context";
 import ImagePicker, { Photo } from "../components/ImagePicker";
+import Progress from "../components/Progress";
+import { Pedometer } from "@ionic-native/pedometer";
+import { getFriendlyTimeOfDay, getFriendlyWalkDescriptor } from "../helpers";
 
 const NewWalk: React.FC = () => {
+  const [isWalking, setIsWalking] = useState(false);
+  const [walkTitle, setWalkTitle] = useState(
+    `${getFriendlyTimeOfDay()} ${getFriendlyWalkDescriptor()}`
+  );
+
+  const [steps, setSteps] = useState(0);
+  const [distance, setDistance] = useState(0);
+
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+
   const [takenPhoto, setTakenPhoto] = useState<Photo>();
   const [chosenWalkType, setChosenWalkType] = useState<WalkType>("user");
 
   const walksCtx = useContext(WalksContext);
-
-  const titleRef = useRef<HTMLIonInputElement>(null);
 
   const history = useHistory();
 
@@ -41,64 +48,103 @@ const NewWalk: React.FC = () => {
     setChosenWalkType(selectedWalkType);
   };
 
-  const addWalkHandler = async () => {
-    const enteredTitle = titleRef.current?.value;
+  const startWalkHandler = () => {
+    setIsWalking(true);
+    setStartTime(new Date().toISOString());
 
-    if (
-      !enteredTitle ||
-      enteredTitle.toString().trim().length === 0 ||
-      !takenPhoto ||
-      !chosenWalkType
-    ) {
+    Pedometer.startPedometerUpdates().subscribe((data) => {
+      setSteps(data.numberOfSteps);
+      setDistance(data.distance / 1000); // metres to km
+    });
+  };
+
+  const endWalkHandler = () => {
+    setIsWalking(false);
+    setEndTime(new Date().toISOString());
+    Pedometer.stopPedometerUpdates();
+
+    if (!walkTitle || !takenPhoto || !chosenWalkType) {
       return;
     }
 
-    walksCtx.addWalk(takenPhoto, enteredTitle.toString(), chosenWalkType);
+    walksCtx.addWalk(takenPhoto, walkTitle, chosenWalkType);
     history.length > 0 ? history.goBack() : history.replace("/user-walks");
   };
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonBackButton defaultHref="/user-walks" />
-          </IonButtons>
-          <IonTitle>Add New Walk</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent>
-        <IonGrid>
-          <IonRow>
-            <IonCol>
-              <IonItem>
-                <IonLabel position="floating">Walk Title</IonLabel>
-                <IonInput type="text" ref={titleRef}></IonInput>
-              </IonItem>
-            </IonCol>
-          </IonRow>
-          <IonRow>
-            <IonCol>
-              <IonSelect
-                onIonChange={selectWalkTypeHandler}
-                value={chosenWalkType}
-              >
-                <IonSelectOption value="user">User Walk</IonSelectOption>
-                <IonSelectOption value="guided">Guided Walk</IonSelectOption>
-              </IonSelect>
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-text-center">
-            <IonCol>
-              <ImagePicker onImagePick={photoPickHandler} />
-            </IonCol>
-          </IonRow>
-          <IonRow className="ion-margin-top">
-            <IonCol className="ion-text-center">
-              <IonButton onClick={addWalkHandler}>Add Walk</IonButton>
-            </IonCol>
-          </IonRow>
-        </IonGrid>
+        {!isWalking ? (
+          <IonGrid>
+            <IonRow>
+              <IonCol className="ion-text-center">
+                <h2>What do you notice as you walk?</h2>
+                <p>
+                  The app will record your journey as you walk. Please stop
+                  anytime to record anything that draws your attention, or that
+                  you see or hear. These will act as markers to guide others
+                  along the route. This might be the taking of a photo, the
+                  recording of a sound, or something written. Notes can be added
+                  such as an observation, a direction about a turn to take, or
+                  anything else that marks a particular spot on your route. You
+                  can edit once you’ve finished the walk before then finally
+                  uploading to the app.
+                </p>
+                <IonSelect
+                  onIonChange={selectWalkTypeHandler}
+                  value={chosenWalkType}
+                >
+                  <IonSelectOption value="user">User Walk</IonSelectOption>
+                  <IonSelectOption value="guided">Guided Walk</IonSelectOption>
+                </IonSelect>
+                <IonItem>
+                  <IonLabel position="floating">
+                    Give this walk a title...
+                  </IonLabel>
+                  <IonInput
+                    type="text"
+                    value={walkTitle}
+                    onIonChange={(event) => setWalkTitle(event.detail!.value!)}
+                  ></IonInput>
+                </IonItem>
+                <IonButton
+                  className="ion-margin-top"
+                  disabled={walkTitle === ""}
+                  onClick={() => startWalkHandler()}
+                >
+                  Start Walk
+                </IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        ) : (
+          <IonGrid>
+            <Progress distance={distance} steps={steps} />
+            Start: {startTime}
+            End: {endTime}
+            <IonRow>
+              <IonCol>
+                <IonSelect
+                  onIonChange={selectWalkTypeHandler}
+                  value={chosenWalkType}
+                >
+                  <IonSelectOption value="user">User Walk</IonSelectOption>
+                  <IonSelectOption value="guided">Guided Walk</IonSelectOption>
+                </IonSelect>
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-text-center">
+              <IonCol>
+                <ImagePicker onImagePick={photoPickHandler} />
+              </IonCol>
+            </IonRow>
+            <IonRow className="ion-margin-top">
+              <IonCol className="ion-text-center">
+                <IonButton onClick={endWalkHandler}>End Walk</IonButton>
+              </IonCol>
+            </IonRow>
+          </IonGrid>
+        )}
       </IonContent>
     </IonPage>
   );
