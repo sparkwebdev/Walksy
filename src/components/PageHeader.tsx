0abc662f100b8932cd4abc52b86cfa5891f9c785
Steven Park <info@stevenpark.co.uk>
2021-02-18 09:39:43 +0000

    PageHeader — back button and action button functionality

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,17 +1,54 @@
-import { IonHeader, IonTitle, IonToolbar } from "@ionic/react";
+import {
+  IonBackButton,
+  IonButton,
+  IonButtons,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { attachProps } from "@ionic/react/dist/types/components/utils";
+import { useLocation } from "react-router-dom";
 
 interface ContainerProps {
   title: string;
+  back?: boolean;
+  showTool?: boolean;
+  toolText?: string;
+  toolAction?: () => void;
 }
 
-const PageHeader: React.FC<ContainerProps> = ({ title }) => {
+const PageHeader: React.FC<ContainerProps> = ({
+  title,
+  back = false,
+  showTool,
+  toolText,
+  toolAction,
+}) => {
+  const location = useLocation();
   return (
     <IonHeader>
       <IonToolbar>
-        <IonTitle>{title}</IonTitle>
+        {back && (
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/app/home" />
+          </IonButtons>
+        )}
+        <IonTitle className="ion-text-center">{title}</IonTitle>
+        {location.pathname === "/app/walk" && showTool && toolAction && (
+          <IonButtons slot="end">
+            <IonButton>
+              <span
+                onClick={() => {
+                  toolAction();
+                }}
+              >
+                {toolText}
+              </span>
+            </IonButton>
+          </IonButtons>
+        )}
       </IonToolbar>
     </IonHeader>
   );
 };
-
 export default PageHeader;
