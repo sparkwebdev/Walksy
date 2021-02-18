f6a874ed3c61dae6dd66a30dd4f21656da60933f
Steven Park <info@stevenpark.co.uk>
2021-02-18 09:47:32 +0000

    Tweak Progress component appearence

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,4 +1,4 @@
-import { IonCard, IonItem, IonIcon } from "@ionic/react";
+import { IonCard, IonItem, IonIcon, IonList } from "@ionic/react";
 import React from "react";
 import "./Progress.css";
 import { time as timeIcon, walk as walkIcon } from "ionicons/icons";
@@ -17,18 +17,20 @@ const Progress: React.FC<ContainerProps> = ({
 }) => {
   return (
     <IonCard className="progress-panel">
-      <IonItem className="progress-panel__item">
-        <IonIcon slot="start" icon={timeIcon} color="primary-contrast" />
-        {time["min"]}&nbsp;<span className="smallprint">min</span>&nbsp;
-        {("0" + time["sec"]).slice(-2)}&nbsp;
-        <span className="smallprint">sec</span>
-      </IonItem>
-      <IonItem className="progress-panel__item">
-        <IonIcon slot="start" icon={walkIcon} color="primary-contrast" />
-        {distance?.toFixed(1)}
-        <span className="smallprint">&nbsp;{getUnitDistance()}</span>&nbsp;— 
-        {steps}&nbsp;<span className="smallprint">steps</span>
-      </IonItem>
+      <IonList lines="none">
+        <IonItem className="progress-panel__item">
+          <IonIcon slot="start" icon={timeIcon} color="primary-contrast" />
+          {time["min"]}&nbsp;<span className="smallprint">min</span>&nbsp;
+          {("0" + time["sec"]).slice(-2)}&nbsp;
+          <span className="smallprint">sec</span>
+        </IonItem>
+        <IonItem className="progress-panel__item">
+          <IonIcon slot="start" icon={walkIcon} color="primary-contrast" />
+          {distance?.toFixed(1)}
+          <span className="smallprint">&nbsp;{getUnitDistance()}</span>&nbsp;— 
+          {steps}&nbsp;<span className="smallprint">steps</span>
+        </IonItem>
+      </IonList>
     </IonCard>
   );
 };
