970216a61c84da866c0c89a96770174ff429975a
Steven Park <info@stevenpark.co.uk>
2021-02-19 13:23:45 +0000

    NotFound update — add back button, tidy content

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -3,20 +3,34 @@ import {
   IonContent,
   IonHeader,
   IonTitle,
-  IonToolbar
-} from '@ionic/react';
-import React from 'react';
+  IonToolbar,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardContent,
+  IonText,
+} from "@ionic/react";
+import React from "react";
+import PageHeader from "../components/PageHeader";
 
 const NotFound: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Not found</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <PageHeader title="Not found" back={true} />
       <IonContent className="ion-padding">
-        Sorry, not found.
+        <IonCard>
+          <IonCardHeader className="ion-no-padding" color="tertiary">
+            <IonCardSubtitle className="ion-padding ion-no-margin ion-text-uppercase ion-text-center">
+              Not found.
+            </IonCardSubtitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <p>
+              <br />
+              Sorry, we couldn't find anything at this location.
+            </p>
+          </IonCardContent>
+        </IonCard>
       </IonContent>
     </IonPage>
   );
