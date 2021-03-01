c15ff21c99eae5039437359d460f672f5a736ceb
Steven Park <info@stevenpark.co.uk>
2021-03-01 11:28:27 +0000

    MomentItemPreview component

---
+++ src/components/MomentItemPreview.tsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import { IonCard, IonCardContent, IonText } from "@ionic/react";
+
+import "./WalkItem.css";
+import { firestore } from "../firebase";
+import { toWalk, Walk } from "../data/models";
+
+const MomentItemPreview: React.FC<{
+  walkId?: string;
+  coverImage?: string;
+}> = (props) => {
+  const [walk, setWalk] = useState<Walk>();
+
+  useEffect(() => {
+    const walkRef = firestore.collection("users-walks").doc(props.walkId);
+    walkRef.get().then((doc) => {
+      setWalk(toWalk(doc));
+    });
+  }, [props.walkId]);
+
+  return (
+    <>
+      <IonCard className="walk-item ion-no-margin">
+        {props.coverImage && walk?.title && (
+          <img
+            className="walk-item__cover-image"
+            src={props.coverImage}
+            alt={walk.title}
+          />
+        )}
+        <IonCardContent className="walk-item__content">
+          <IonText className="text-heading">
+            {walk?.title && (
+              <h2>
+                <strong>{walk.title}</strong>
+              </h2>
+            )}
+            {walk?.description && (
+              <p>
+                <strong>{walk.description}</strong>
+              </p>
+            )}
+          </IonText>
+        </IonCardContent>
+      </IonCard>
+    </>
+  );
+};
+
+export default MomentItemPreview;
