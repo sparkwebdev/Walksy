8c63fec859f08b3dfdb96f2edecbff253544ad0f
Steven Park <info@stevenpark.co.uk>
2021-03-01 11:25:13 +0000

    Update Homepage — data from firebase; (behaves as Browse Gallery page)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,51 @@
-import { IonPage, IonContent } from "@ionic/react";
-import React from "react";
+import {
+  IonPage,
+  IonContent,
+  IonGrid,
+  IonCol,
+  IonRow,
+  IonCard,
+} from "@ionic/react";
+import React, { useEffect, useState } from "react";
+import MomentItemPreview from "../components/MomentItemPreview";
 import PageHeader from "../components/PageHeader";
-import WalksList from "../components/WalksList";
+import { Moment, toMoment } from "../data/models";
+import { firestore } from "../firebase";
 
 const HomePage: React.FC = () => {
+  const [moments, setMoments] = useState<Moment[]>([]);
+
+  useEffect(() => {
+    const momentsRef = firestore
+      .collection("users-moments")
+      .where("imagePath", "!=", "");
+    return momentsRef.orderBy("imagePath").onSnapshot(({ docs }) => {
+      setMoments(docs.map(toMoment));
+    });
+  }, []);
+
   return (
     <IonPage>
-      <PageHeader title="Home" />
+      <PageHeader title="Browse Gallery" />
       <IonContent>
-        <WalksList title="" type="user" />
-        <WalksList title="" type="guided" />
+        <IonGrid className="ion-no-padding">
+          <IonRow className="grid-new">
+            {moments.map((moment, index) => (
+              <IonCol className="grid-new__item">
+                <IonCard
+                  className="ion-no-margin ion-no-padding"
+                  key={index}
+                  href={`/app/walk/${moment.walkId}`}
+                >
+                  <MomentItemPreview
+                    walkId={moment.walkId}
+                    coverImage={moment.imagePath}
+                  />
+                </IonCard>
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
       </IonContent>
     </IonPage>
   );
