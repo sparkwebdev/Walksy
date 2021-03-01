5a6d64c64e4c3f282076e1c17385921dee5b1135
Steven Park <info@stevenpark.co.uk>
2021-03-01 11:30:14 +0000

    WalkEntryPage component

---
+++ src/pages/WalkEntryPage.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { IonContent, IonPage } from "@ionic/react";
+import PageHeader from "../components/PageHeader";
+import WalkItem from "../components/WalkItem";
+import { useParams } from "react-router";
+import { firestore } from "../firebase";
+import { toWalk, Walk } from "../data/models";
+
+interface RouteParams {
+  id: string;
+}
+
+const WalkEntryPage: React.FC = () => {
+  const { id } = useParams<RouteParams>();
+  const [walk, setWalk] = useState<Walk>();
+
+  useEffect(() => {
+    const walkRef = firestore.collection("users-walks").doc(id);
+    walkRef.get().then((doc) => {
+      setWalk(toWalk(doc));
+    });
+  }, [id]);
+
+  return (
+    <IonPage>
+      <PageHeader title="Walk" back={true} />
+      <IonContent>
+        <WalkItem
+          id={id}
+          title={walk?.title}
+          colour={walk?.colour}
+          description={walk?.description}
+          start={walk?.start}
+          end={walk?.end}
+          steps={walk?.steps}
+          distance={walk?.distance}
+          coverImage={walk?.coverImage}
+        />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default WalkEntryPage;
