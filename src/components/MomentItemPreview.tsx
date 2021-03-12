6fbaf29812e6bb85cef701a8b6eaf44f39270725
Steven Park <info@stevenpark.co.uk>
2021-03-12 14:36:35 +0000

    Tweak MomentItemPreview

--- src/components/MomentItemPreview.tsx
+++ src/components/MomentItemPreview.tsx
@@ -19,31 +19,29 @@ const MomentItemPreview: React.FC<{
   }, [props.walkId]);
 
   return (
-    <>
-      <IonCard className="walk-item ion-no-margin">
-        {props.coverImage && walk?.title && (
-          <img
-            className="walk-item__cover-image"
-            src={props.coverImage}
-            alt={walk.title}
-          />
-        )}
-        <IonCardContent className="walk-item__content">
-          <IonText className="text-heading">
-            {walk?.title && (
-              <h2>
-                <strong>{walk.title}</strong>
-              </h2>
-            )}
-            {walk?.description && (
-              <p>
-                <strong>{walk.description}</strong>
-              </p>
-            )}
-          </IonText>
-        </IonCardContent>
-      </IonCard>
-    </>
+    <div className="walk-item ion-no-margin">
+      {props.coverImage && walk?.title && (
+        <img
+          className="walk-item__cover-image"
+          src={props.coverImage}
+          alt={walk.title}
+        />
+      )}
+      <IonCardContent className="walk-item__content">
+        <IonText className="text-heading">
+          {walk?.title && (
+            <h2>
+              <strong>{walk.title}</strong>
+            </h2>
+          )}
+          {walk?.description && (
+            <p>
+              <strong>{walk.description}</strong>
+            </p>
+          )}
+        </IonText>
+      </IonCardContent>
+    </div>
   );
 };
 
