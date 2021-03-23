4b4efa754b0021543740ba1758234705329327f9
Steven Park <info@stevenpark.co.uk>
2021-03-23 15:04:37 +0000

    Tweak Progress layout

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -1,7 +1,20 @@
-import { IonCard, IonItem, IonIcon, IonList } from "@ionic/react";
+import {
+  IonCard,
+  IonItem,
+  IonIcon,
+  IonList,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonText,
+} from "@ionic/react";
 import React, { useEffect, useState } from "react";
 import "./Progress.css";
-import { time as timeIcon, walk as walkIcon } from "ionicons/icons";
+import {
+  timerOutline as timeIcon,
+  arrowUpCircleOutline as distanceIcon,
+  footstepsOutline as walkIcon,
+} from "ionicons/icons";
 import { getMinAndSec, getTimeDiff, getUnitDistance } from "../helpers";
 import { Pedometer } from "@ionic-native/pedometer";
 import { Time } from "../data/models";
@@ -41,8 +54,58 @@ const Progress: React.FC<ContainerProps> = ({ start, updateWalk }) => {
   }, [distance, steps, updateWalk]);
 
   return (
-    <IonCard className="progress-panel">
-      <IonList lines="none">
+    <IonCard className="progress-panel" color="dark">
+      <IonGrid>
+        <IonRow className="ion-justify-content-center">
+          <IonCol
+            className="ion-text-center"
+            style={{
+              fontSize: "1.15em",
+              padding: "10px 0",
+            }}
+          >
+            <IonIcon
+              icon={timeIcon}
+              style={{
+                verticalAlign: "middle",
+                margin: "0 5px 2px 0",
+                fontSize: "1.5em",
+              }}
+            />
+            <span
+              style={{
+                minWidth: "6rem",
+                display: "inline-block",
+                textAlign: "left",
+              }}
+            >
+              {time["min"]}&nbsp;<span className="smallprint">min</span>&nbsp;
+              {("0" + time["sec"]).slice(-2)}&nbsp;
+              <span className="smallprint">sec</span>
+            </span>
+            <IonIcon
+              icon={distanceIcon}
+              style={{
+                verticalAlign: "middle",
+                margin: "0 5px 2px 5%",
+                fontSize: "1.5em",
+              }}
+            />
+            {distance?.toFixed(1)}
+            <span className="smallprint">&nbsp;{getUnitDistance()}</span>
+            <IonIcon
+              icon={walkIcon}
+              style={{
+                verticalAlign: "middle",
+                margin: "0 5px 2px 6%",
+                fontSize: "1.5em",
+              }}
+            />
+            {steps}&nbsp;<span className="smallprint">steps</span>
+          </IonCol>
+        </IonRow>
+      </IonGrid>
+      {/* <IonList lines="none">
         <IonItem className="progress-panel__item">
           <IonIcon slot="start" icon={timeIcon} color="primary-contrast" />
           {time["min"]}&nbsp;<span className="smallprint">min</span>&nbsp;
@@ -55,7 +118,7 @@ const Progress: React.FC<ContainerProps> = ({ start, updateWalk }) => {
           <span className="smallprint">&nbsp;{getUnitDistance()}</span>&nbsp;— 
           {steps}&nbsp;<span className="smallprint">steps</span>
         </IonItem>
-      </IonList>
+      </IonList> */}
     </IonCard>
   );
 };
