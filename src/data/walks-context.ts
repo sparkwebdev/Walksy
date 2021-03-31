cc51c18e6be5909269981b18c90d630bc776a791
Steven Park <info@stevenpark.co.uk>
2021-03-31 14:41:50 +0100

    Add storedWalkId, updateWalkIdForStorage, storeMoments, resetWalk methods to Walk Context

--- src/data/walks-context.ts
+++ src/data/walks-context.ts
@@ -4,19 +4,27 @@ import { Walk, Moment, Location } from "../data/models";
 
 interface Context {
   walk: Walk | {};
+  storedWalkId: string;
+  updateWalkIdForStorage: (walkId: string) => void,
   moments: Moment[];
   updateWalk: ({}) => void;
   addMoment: (walkId: string, imagePath: string, audioPath: string, note: string, location: Location | null, timestamp: string) => void;
   deleteMoment: (momentId: string) => void;
+  storeMoments: (userId: string) => void;
+  resetWalk: () => void;
   reset: () => void;
 }
 
 const WalksContext = React.createContext<Context>({
   walk: {},
+  storedWalkId: "",
+  updateWalkIdForStorage: () => {},
   moments: [],
   updateWalk: () => {},
   addMoment: () => {},
   deleteMoment: () => {},
+  storeMoments: () => {},
+  resetWalk: () => {},
   reset: () => {},
 });
 
