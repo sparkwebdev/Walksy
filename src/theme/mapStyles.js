0b7ee674d0ce36394f08516471f7055fae582049
Steven Park <info@stevenpark.co.uk>
2021-04-03 17:17:57 +0100

    New Map styles

--- src/theme/mapStyles.js
+++ src/theme/mapStyles.js
@@ -1,203 +1,346 @@
 const styles = [
-  {
-      "elementType": "labels",
-      "stylers": [
-          {
-              "visibility": "off"
-          },
-          {
-              "color": "#f49f53"
-          }
-      ]
-  },
-  {
-      "featureType": "landscape",
-      "stylers": [
-          {
-              "color": "#f9ddc5"
-          },
-          {
-              "lightness": -7
-          }
-      ]
-  },
-  {
-      "featureType": "road",
-      "stylers": [
-          {
-              "color": "#813033"
-          },
-          {
-              "lightness": 43
-          }
-      ]
-  },
-  {
-      "featureType": "poi.business",
-      "stylers": [
-          {
-              "color": "#645c20"
-          },
-          {
-              "lightness": 38
-          }
-      ]
-  },
-  {
-      "featureType": "water",
-      "stylers": [
-          {
-              "color": "#1994bf"
-          },
-          {
-              "saturation": -69
-          },
-          {
-              "gamma": 0.99
-          },
-          {
-              "lightness": 43
-          }
-      ]
-  },
-  {
-      "featureType": "road.local",
-      "elementType": "geometry.fill",
-      "stylers": [
-          {
-              "color": "#f19f53"
-          },
-          {
-              "weight": 1.3
-          },
-          {
-              "visibility": "on"
-          },
-          {
-              "lightness": 16
-          }
-      ]
-  },
-  {
-      "featureType": "poi.business"
-  },
-  {
-      "featureType": "poi.park",
-      "stylers": [
-          {
-              "color": "#645c20"
-          },
-          {
-              "lightness": 39
-          }
-      ]
-  },
-  {
-      "featureType": "poi.school",
-      "stylers": [
-          {
-              "color": "#a95521"
-          },
-          {
-              "lightness": 35
-          }
-      ]
-  },
-  {},
-  {
-      "featureType": "poi.medical",
-      "elementType": "geometry.fill",
-      "stylers": [
-          {
-              "color": "#813033"
-          },
-          {
-              "lightness": 38
-          },
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {},
-  {
-      "elementType": "labels"
-  },
-  {
-      "featureType": "poi.sports_complex",
-      "stylers": [
-          {
-              "color": "#9e5916"
-          },
-          {
-              "lightness": 32
-          }
-      ]
-  },
-  {},
-  {
-      "featureType": "poi.government",
-      "stylers": [
-          {
-              "color": "#9e5916"
-          },
-          {
-              "lightness": 46
-          }
-      ]
-  },
-  {
-      "featureType": "transit.station",
-      "stylers": [
-          {
-              "visibility": "off"
-          }
-      ]
-  },
-  {
-      "featureType": "transit.line",
-      "stylers": [
-          {
-              "color": "#813033"
-          },
-          {
-              "lightness": 22
-          }
-      ]
-  },
-  {
-      "featureType": "transit",
-      "stylers": [
-          {
-              "lightness": 38
-          }
-      ]
-  },
-  {
-      "featureType": "road.local",
-      "elementType": "geometry.stroke",
-      "stylers": [
-          {
-              "color": "#f19f53"
-          },
-          {
-              "lightness": -10
-          }
-      ]
-  },
-  {},
-  {},
-  {}
+    {
+        "featureType": "all",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "10"
+            },
+            {
+                "lightness": "0"
+            },
+            {
+                "hue": "#00ff65"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#ffa100"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#00ffe1"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.attraction",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#00b3ff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.business",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.government",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#00b3ff"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.medical",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.place_of_worship",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "hue": "#00ffe1"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.school",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "saturation": "0"
+            },
+            {
+                "lightness": "0"
+            },
+            {
+                "weight": "1"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#0095ff"
+            },
+            {
+                "lightness": "25"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
 ]
 
 export default styles;
