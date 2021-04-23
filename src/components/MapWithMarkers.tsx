4a5c7b703c853aab4a2b5b23853930c280032a56
Steven Park <info@stevenpark.co.uk>
2021-04-23 10:30:15 +0100

    Remove 'current location' marker

--- src/components/MapWithMarkers.tsx
+++ src/components/MapWithMarkers.tsx
@@ -133,7 +133,7 @@ const MapWithMarkers: React.FC<{
                 anchor: new window.google.maps.Point(15, 30),
               }}
             />
-            <Marker
+            {/* <Marker
               position={{
                 lat: props.locations![props.locations.length - 1].lat,
                 lng: props.locations![props.locations.length - 1].lng,
@@ -144,7 +144,7 @@ const MapWithMarkers: React.FC<{
                 origin: new window.google.maps.Point(0, 0),
                 anchor: new window.google.maps.Point(10, 10),
               }}
-            />
+            /> */}
           </>
         )}
 
