166205730fcff9a62249f4cfdd2199ec853ebbbd
Steven Park <info@stevenpark.co.uk>
2021-04-30 09:59:46 +0100

    Add PrivacyPolicy

---
+++ src/pages/PrivacyPage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { IonContent, IonPage } from "@ionic/react";
+import PageHeader from "../components/PageHeader";
+import PrivacyPolicy from "../components/PrivacyPolicy";
+
+const CopyrightPage: React.FC = () => {
+  return (
+    <IonPage>
+      <PageHeader title="Privacy Policy" back={true} />
+      <IonContent className="ion-padding">
+        <div
+          className="constrain constrain--large ion-margin-top"
+          style={{ lineHeight: 2 }}
+        >
+          <PrivacyPolicy />
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default CopyrightPage;
