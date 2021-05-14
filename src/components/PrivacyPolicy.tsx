9770443210fba3c9408417c56827c201b5f647cf
Steven Park <info@stevenpark.co.uk>
2021-05-14 09:59:28 +0100

    New Privacy Policy

--- src/components/PrivacyPolicy.tsx
+++ src/components/PrivacyPolicy.tsx
@@ -4,31 +4,48 @@ const PrivacyPolicy: React.FC = () => {
   return (
     <>
       <p>
-        Art Walk Projects is committed to ensuring that your information is
-        secure whilst using this App. We will not sell, distribute or lease your
-        personal information onto any third party. We will only use your
-        personal information to occasionally send you promotional information
-        about our work and future plans relating to the App which we think you
-        may find of interest.
+        Art Walk Projects is committed to safeguarding the privacy of all our
+        App &amp; Website users. We will not sell, distribute or lease your
+        personal information onto any third party.
       </p>
       <p>
-        When you register at Walksy, we store personal information only such as
-        your name and email address. Any information about age is gathered
-        simply to record the breadth of use of the app. You can edit any of this
-        information should you wish to at any time through the profile section
-        of the App. If you wish to unregister at any time you can do so by
-        emailing{" "}
+        When you register at Walksy, we store personal information only, such as
+        your name and email address for the sole purpose of registering and
+        using the App. Any information about age is gathered simply to record
+        the breadth of use of the App. You can edit any of this information
+        should you wish to at any time through the profile section of the App.
+        If you wish to unregister at any time you can do so by emailing
         <a href="mailto:walksy@artwalkporty.co.uk">walksy@artwalkporty.co.uk</a>
         .
       </p>
+
       <p>
-        We will store your personal information as securely as possible (both in
-        terms of procedures and technology used). We use mailchimp to store all
-        contact information which is administered via a secure server.
+        The Walksy. App will collect location data information through your
+        device whilst you are using it to measure and track your walk’s
+        progress, and the location of the individual ‘Moments’ you add to your
+        walk.
       </p>
+      <p>
+        We will store your data as securely as possible (both in terms of
+        procedures and technology used) administered via a secure server. In
+        order to prevent unauthorised access or disclosure SSL (Security Sockets
+        Layer) encryption technology is in place to encrypt sensitive
+        information.
+      </p>
+
       <p>
         Should you unsubscribe at any time your personal information will no
         longer be held in our database.
+      </p>
+
+      <p>
+        Art Walk Projects is a community interest community registered in
+        Scotland SC525677 with our registered address: 1/1 Kings Place Edinburgh
+        EH15 1DU
+      </p>
+
+      <p>
+        <a href="https://walksy.uk/privacy">View our full Privacy Policy</a>.
       </p>
     </>
   );
