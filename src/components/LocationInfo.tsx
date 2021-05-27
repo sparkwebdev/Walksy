611420758e7f0f87e8a629bdde100e960fee136d
Steven Park <info@stevenpark.co.uk>
2021-05-27 18:46:14 +0100

    Tweak LocationInfo layout

--- src/components/LocationInfo.tsx
+++ src/components/LocationInfo.tsx
@@ -5,25 +5,31 @@ import React from "react";
 const LocationInfo: React.FC<{
   lat?: number;
   lng?: number;
-  timestamp?: number;
+  timestamp?: string;
 }> = (props) => {
   return (
     <IonGrid className="ion-text-start">
       <IonRow>
         <IonCol>
-          <small>Latitude: </small>
-          <br />
-          {props.lat}
+          <small>
+            Latitude:
+            <br />
+            {props.lat}{" "}
+          </small>
         </IonCol>
         <IonCol>
-          <small>Longitude: </small>
-          <br />
-          {props.lng}
+          <small>
+            Longitude:
+            <br />
+            {props.lng}{" "}
+          </small>
         </IonCol>
-        <IonCol>
-          <small>Timestamp: </small>
-          <br />
-          {dayjs(props.timestamp).format("HH:mm:ss DD/MM/YY")}
+        <IonCol size="5">
+          <small>
+            Timestamp:
+            <br />
+            {dayjs(props.timestamp).format("DD/MM/YY HH:mm")}
+          </small>
         </IonCol>
       </IonRow>
     </IonGrid>
